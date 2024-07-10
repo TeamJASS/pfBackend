6f4d9f85c749a05152ca28231cc3a8e5851be8d4
Sarah Asare-Darko <saradarko22@gmail.com>
2024-07-10 12:16:48 +0000

    User SignUp

---
+++ controllers/user_controller.js
@@ -0,0 +1,10 @@
+import { User } from "../models/user.js";
+import { userSchema } from "../schema/user_schema.js";
+
+
+export const signUp =(req,res)=> {
+const {error,value}= userSchema.validate(req.body)
+if (error){
+    return res.status(400).send(error.details[0].message)
+}
+}
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
-
+import userRouter from "./Routes/user_routes.js";
 
 //DB Connection String
 await mongoose.connect(process.env.MONGO_URI)
+console.log("Database is connected")
 
 // use express
 const app = express();
 
+// use middleware
+app.use(express.json());
+
+app.use("/api/v1",userRouter)
 
 
 
